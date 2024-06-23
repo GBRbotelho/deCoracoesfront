36d5679813d834b81a3933824017f03eed6c6810
GBRbotelho <gabrielbbotelho05@gmail.com>
2024-06-23 17:32:06 -0300

    Login #1

--- src/pages/Home/@Index.jsx
+++ src/pages/Home/@Index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./@Index.module.css";
 import Navbar from "./Containers/1Navbar";
 import Section1 from "./Containers/Section1";
@@ -8,12 +8,15 @@ import Section4Funcionamento from "./Containers/Section4Funcionamento";
 import Section5Fotos from "./Containers/Section5Fotos";
 import Section7Perguntas from "./Containers/Section7Perguntas";
 import Section6 from "./Containers/Section6";
+import Login from "../../components/Modals/Login";
 
 export default function Index() {
+  const [modalLogin, setModalLogin] = useState(false);
+
   return (
     <main className={styles.main}>
       <div className={styles.div1}>
-        <Navbar />
+        <Navbar setModalLogin={setModalLogin} />
         <Section1 />
       </div>
       <Section2Box />
@@ -22,6 +25,7 @@ export default function Index() {
       <Section5Fotos />
       <Section6 />
       <Section7Perguntas />
+      {modalLogin && <Login setState={setModalLogin} />}
     </main>
   );
 }
--- src/pages/Home/Containers/1Navbar.jsx
+++ src/pages/Home/Containers/1Navbar.jsx
@@ -3,9 +3,12 @@ import styles from "./1Navbar.module.css";
 import Hamburger from "../../../components/Navbar/Hamburger";
 import Close from "../../../components/Navbar/Close";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../../contexts/AuthContext";
 
-export default function Navbar() {
+export default function Navbar({ setModalLogin }) {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const { user } = useAuth();
 
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
@@ -29,8 +32,89 @@ export default function Navbar() {
           <li onClick={() => scrollToSection("planos")}>planos</li>
         </ul>
       </nav>
-      <div className={styles.button}>
-        <button>entrar</button>
+      <div
+        className={styles.button}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {!user ? (
+          <button
+            className="flex items-center gap-2"
+            onClick={() => setModalLogin(true)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="h-5 w-5 fill-primary-foreground"
+            >
+              <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+            Entrar
+          </button>
+        ) : (
+          <div className="relative">
+            <button className="flex gap-2 items-center">
+              Gabriel
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 15l5-5m0 0l-5-5m5 5H3"
+                />
+              </svg>
+            </button>
+            <div
+              className={`absolute transform transition-all duration-300 ${
+                isHovered
+                  ? "opacity-100 -translate-x-full"
+                  : "opacity-0 translate-x-0"
+              } left-full top-[120%] bg-white rounded-lg overflow-hidden`}
+            >
+              <ul className="m-2">
+                <li className="cursor-pointer px-10 hover:bg-slate-200">
+                  Pedidos
+                </li>
+                <li className="cursor-pointer px-10 hover:bg-slate-200">
+                  Configurações
+                </li>
+                <li className="cursor-pointer px-10 hover:bg-slate-200 flex gap-2 items-center">
+                  Sair
+                  <svg
+                    className="w-4 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+                    <polyline points="16 17 21 12 16 7" />
+                    <line x1="21" x2="9" y1="12" y2="12" />
+                  </svg>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
       <nav className={styles.navMobile}>
         <button
